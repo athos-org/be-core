1fd9439a6ee65dea2ad75d792226b3f84c671b89
sabazed <zedginidze.saba@outlook.com>
2025-09-10 01:51:37 +0400

    Refactor InternalApiService to use SecureStoreService

--- src/main/java/org/athos/core/service/InternalApiService.java
+++ src/main/java/org/athos/core/service/InternalApiService.java
@@ -6,7 +6,7 @@ import com.github.benmanes.caffeine.cache.RemovalCause;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import one.util.streamex.StreamEx;
-import org.athos.core.service.secure.VaultSecureStoreService;
+import org.athos.core.service.secure.SecureStoreService;
 import org.springframework.stereotype.Service;
 
 import java.net.InetAddress;
@@ -22,7 +22,7 @@ public class InternalApiService {
   private static final List<String> INTERNAL_SERVICE_NAMES = List.of("kong", "athos-bootstrap"); // TODO: Fetch from configuration
   private static final String INTERNAL_API_KEY = "internal-api-key";
 
-  private final VaultSecureStoreService vaultStoreService;
+  private final SecureStoreService vaultStoreService;
   private Cache<String, String> currentKeyCache;
   private Cache<String, String> previousKeyCache;
   private Cache<String, String> gatewayAddressCache;
