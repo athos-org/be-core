12710003b38708961b358997db0d99244852ee72
Saba Zedginidze <zedginidze.saba@outlook.com>
2025-09-30 00:28:49 +0400

    Add mappings for new metadata version field used for optimistic locking (#7)

--- src/main/java/org/athos/core/domain/mapper/EntityMetadataMapping.java
+++ src/main/java/org/athos/core/domain/mapper/EntityMetadataMapping.java
@@ -6,6 +6,7 @@ import org.mapstruct.Mapping;
 @Mapping(target = "createdDate", ignore = true)
 @Mapping(target = "updatedBy", ignore = true)
 @Mapping(target = "updatedDate", ignore = true)
+@Mapping(target = "version", source = "metadata.version")
 public @interface EntityMetadataMapping {
 
 }
